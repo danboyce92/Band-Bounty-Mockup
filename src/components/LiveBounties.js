1518e6b616385b2aa51eb7f9e34c854410c6e590
Daniel Boyce <daarf92@gmail.com>
2023-01-26 15:01:07 +0000

    Timer finished now modal work

--- src/components/LiveBounties.js
+++ src/components/LiveBounties.js
@@ -8,7 +8,7 @@ const LiveBounties = () => {
 
     const [ bounties, setBounties ] = useState([]);
 
-    console.log(bounties[0]?.expiration.seconds)
+
 
     useEffect(() => {
         const unsub = onSnapshot(collection(db, "Bounties"), (snapshot) => {
@@ -21,13 +21,13 @@ const LiveBounties = () => {
         <div>
             <div className="relative ml-4 flex items-center">
                
-                <div className=" mx-auto my-4 bg-white w-11/12 max-w-6xl h-16 rounded-md flex flex-row divide-x-2 divide-slate-400">
+                <div className=" mx-auto my-4 bg-white w-11/12 max-w-6xl h-16 rounded-md flex flex-row divide-x-4 divide-slate-400">
                     <div className="basis-2/12 text-center mt-5">Artist</div>
                     <div className="basis-2/12 text-center mt-5">City</div>
                     <div className="basis-2/12 text-center mt-5">Funds raised</div>
                     <div className="basis-2/12 text-center mt-5">Target</div>
-                    <div className="basis-2/12 text-center mt-5">Expiration timer</div>
-                    <div className="basis-2/12 text-center mt-5"></div>
+                    <div className="basis-4/12 text-center mt-5">Expiration timer</div>
+
                 </div>
                 </div>
 
@@ -35,12 +35,14 @@ const LiveBounties = () => {
             bounties.map((bounty) => (
                 <div key={bounty.artist} className="ml-4 relative flex flex-col">
 
-                    <div className={`${bounty.active && "border-4 border-green-300"} ${!bounty.active && "border-4 border-red-600"} mx-auto my-3 bg-white w-11/12 max-w-6xl h-16 rounded-md flex flex-row divide-x-2 divide-slate-400`}>
+                    <div className={`${bounty.active && "border-4 bg-green-300"} ${!bounty.active && "border-4 bg-red-600"} mx-auto my-3  w-11/12 max-w-6xl h-16 rounded-md flex flex-row divide-x-2 divide-slate-400`}>
                         <div className="basis-2/12 text-center mt-5">{bounty.artist}</div>
                         <div className="basis-2/12 text-center mt-5">{bounty.city}</div>
                         <div className="basis-2/12 text-center mt-5">{bounty.funds}</div>
                         <div className="basis-2/12 text-center mt-5">{bounty.target}</div>
-                        {/* <div className="basis-2/12 text-center mt-5">{<Timer date={bounty.expiration.seconds} />}</div> */}
+                        {bounty.expiration &&
+                        <div className="basis-4/12 text-center mt-5">{<Timer date={bounty.expiration.seconds} />} <div id="time-left"></div></div>
+                        }
                     </div>
 
                 </div>
@@ -48,6 +50,9 @@ const LiveBounties = () => {
             ))
             
             }
+
+
+            <button className="bg-orange-400 right-4 float-right mr-14 py-4 px-8 rounded-xl font-medium hover:bg-orange-200">Create Bounty</button>
 
 
         </div>
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,43 +1,85 @@
-import React from "react";
+import React, { useState } from "react"
+import { useInterval } from "./useInterval"
 
 const Timer = ({ date }) => {
 
-const timeLeft = document.getElementById('time-left')
+    let [ display, setDisplay ] = useState()
+    let [ isRunning, setIsRunning ] = useState(true)
 
-const deadline = new Date('02/04/23')
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
-let timerId
 
-function countDown() {
-    const today = new Date()
-    const timeSpan = deadline - today
+    const deadline = date * 1000
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    useInterval(() => {
+        
+        const today = new Date()
+        const timeSpan = deadline - today
+           
+        if (timeSpan <= 0) {
+            setDisplay("Bounty has expired")
+            setIsRunning(false)
+            return
+        }
+    
+        const days = Math.floor(timeSpan / day)
+        const hours = Math.floor((timeSpan % day) / hour)
+        const minutes = Math.floor((timeSpan % hour) / minute)
+        const seconds = Math.floor((timeSpan % minute) / second)
+    
+        if(days > 0){
+             setDisplay(days + ' days ' + hours + 'hours')
+        } if (days <= 0 && hours > 0) {
+            setDisplay(hours + ':' + minutes + ':' + seconds)
+        } if (hours <= 0 && seconds > 0) {
+            setDisplay(minutes + ':' + seconds)
+        }
+    
+    }, isRunning ? 1000 : null);
+
+
+
+
+// const deadline = date * 1000
+// const second = 1000;
+// const minute = second * 60;
+// const hour = minute * 60;
+// const day = hour * 24;
+// let timerId
+
+// const countDown = () => {
+//     const today = new Date()
+//     const timeSpan = deadline - today
     
 
     
-    if (timeSpan <= 0) {
-        timeLeft.innerHTML = "00:00"
-        clearInterval(timerId)
-        return
-    }
+//     if (timeSpan <= 0) {
+//         timeLeft.innerHTML = "Bounty has expired"
+//         clearInterval(timerId)
+//         return
+//     }
 
-    const days = Math.floor(timeSpan / day)
-    const hours = Math.floor((timeSpan % day) / hour)
-    const minutes = Math.floor((timeSpan % hour) / minute)
-    const seconds = Math.floor((timeSpan % minute) / second)
+//     const days = Math.floor(timeSpan / day)
+//     const hours = Math.floor((timeSpan % day) / hour)
+//     const minutes = Math.floor((timeSpan % hour) / minute)
+//     const seconds = Math.floor((timeSpan % minute) / second)
 
-    timeLeft.innerHTML = days + 'days' + hours + 'hours' + minutes + 'min' + seconds + "seconds"
+//     if(days > 0){
+//         timeLeft.innerHTML = days + ' days ' + hours + 'hours'
+//     } if (days <= 0 && hours > 0) {
+//         timeLeft.innerHTML = hours + ':' + minutes + ':' + seconds
+//     } if (hours <= 0 && seconds > 0) {
+//         timeLeft.innerHTML = minutes + ':' + seconds
+//     }
 
-}
-
-timerId = setInterval(countDown, second)
+// }
 
 
 return (
-    <div id="time-left">
-        
+    <div>
+        {display}
     </div>
 )
 

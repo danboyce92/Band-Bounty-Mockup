8bf59e348b2a1369c28eab286eb286a937070195
Daniel Boyce <daarf92@gmail.com>
2023-02-11 15:47:52 +0000

    contact page refactored

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,46 +35,51 @@ const Contact = () => {
 
   return (
     <div>
-      <div className="w-7/12 mx-auto mt-24 h-full  grid grid-rows-6 grid-cols-10 gap-4 bg-gray-200 rounded-md pb-8">
-        <span className="col-start-5 col-span-2 row-span-1 text-center mt-2 text-[32px] font-bold">
-          Contact us
-        </span>
-        <form className="col-start-4 row-start-2 col-span-4 row-span-5 divide-y-8">
-          <div className="col-start-4 row-start-2 row-span-1">
-            <label className="mx-auto">Email address</label>
-
-            <input
-              onChange={handleEmailChange}
-              placeholder="Enter your email address here.."
-              type="text"
-              size="45"
-              className="py-3 px-2 rounded-md"
-            />
-          </div>
-          <br />
-          <div className="col-start-4 row-start-3">
-            <label>Reason</label>
-            <Dropdown
-              options={options}
-              value={selection}
-              onChange={handleSelect}
-            />
-          </div>
-          <br />
-          <div className="col-start-4 row-start-4 row-span-3">
-            <label>Message</label>
-            <textarea
-              onChange={handleMessageChange}
-              id="text-area"
-              type="text"
-              placeholder="Enter message here.."
-              className="rounded-md px-2 py-3"
-            />
-          </div>
-        </form>
-        <button className="col-start-8 row-start-6 col-span-2 bg-indigo-600 rounded-xl text-white m-auto px-8 py-3">
-          Send
-        </button>
+      <div className="w-7/12 min-w-[550px] max-w-[700px] mx-auto mt-24 h-full  bg-gray-200 rounded-md pb-8">
+        <div className="p-12">
+          <span className="flex justify-center text-[32px] font-bold py-4">
+            Contact us
+          </span>
+          <form className="px-24">
+            <div className="py-4">
+              <label className="">Email address</label>
+              <br />
+              <input
+                onChange={handleEmailChange}
+                placeholder="Enter your email address here.."
+                type="text"
+                size="35"
+                className="py-3 px-2 rounded-md"
+              />
+            </div>
+            <br />
+            <div className="">
+              <label>Reason</label>
+              <Dropdown
+                options={options}
+                value={selection}
+                onChange={handleSelect}
+              />
+            </div>
+            <br />
+            <div className="py-4">
+              <label>Message</label>
+              <br />
+              <textarea
+                onChange={handleMessageChange}
+                id="text-area"
+                type="text"
+                cols={38}
+                rows={4}
+                placeholder="Enter message here.."
+                className="rounded-md px-2 py-3 "
+              />
+            </div>
+          </form>
+          <button className=" bg-indigo-600 rounded-xl text-white m-auto px-8 py-3 float-right mb-8">
+            Send
+          </button>
+        </div>
       </div>
     </div>
   );
